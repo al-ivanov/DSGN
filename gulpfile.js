8e0512fac4a3897b3c222a92812d7284040e9b25
Alexandr <saanyyaaa@gmail.com>
2019-03-03 23:05:35 +0300

    DSGN Project

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+'use strict';
+
+var gulp = require('gulp');
+var del = require('del');
+var htmlmin = require('gulp-htmlmin');
+var rigger = require('gulp-rigger');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssmin = require('gulp-csso');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var imagemin = require('gulp-imagemin');
+var browserSync = require("browser-sync").create();
+
+gulp.task('clean', function () {
+    return del('dist');
+});
+
+gulp.task('copy', function () {
+    return gulp.src('assets/fonts/*.{woff,woff2}', {
+            base: './assets/'
+        })
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('image', function () {
+    return gulp.src('assets/img/**/*.{png,jpg,svg}', {
+            base: './assets/'
+        })
+        .pipe(imagemin([
+            imagemin.optipng({
+                optimizationLevel: 3
+            }),
+            imagemin.jpegtran({
+                progressive: true
+            }),
+            imagemin.svgo()
+        ]))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html', function () {
+    return gulp.src('assets/*.html')
+        .pipe(rigger())
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('style', function () {
+    return gulp.src('assets/css/style.css', {
+            base: './assets/'
+        })
+        .pipe(rigger())
+        .pipe(postcss([autoprefixer({
+            grid: true
+        })]))
+        .pipe(cssmin())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('js', function (cb) {
+    pump([
+            gulp.src('assets/js/*.js', {
+                base: './assets/'
+            }),
+            uglify(),
+            gulp.dest('dist')
+        ],
+        cb
+    );
+});
+
+gulp.task('default',
+    gulp.series('clean',
+        gulp.parallel('copy', 'image', 'html', 'style', 'js'),
+    )
+);
+
+gulp.task('dev', function () {
+    browserSync.init({
+        server: 'dist'
+    });
+    gulp.watch('assets/css/*.css', gulp.series('style'));
+    gulp.watch('assets/js/*.js', gulp.series('js'));
+    gulp.watch('assets/*.html', gulp.series('html'));
+    gulp.watch('assets/img/**/*.{png,jpg,svg}', gulp.series('image'));
+    browserSync.watch('./**/*.*').on('change', browserSync.reload);
+});
